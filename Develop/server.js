a0cd2b7ad3a2117f3d462a5e6071fbe95dc57170
Kevin Hanson <ziffoit@gmail.com>
2021-04-28 21:37:55 -0600

    server

---
+++ Develop/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes')
+const htmlRoutes = require('./routes/htmlRoutes')
+// const path = require('path');
+
+// Sets up the Express App
+
+const app = express();
+const PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'))
+
+app.use(apiRoutes)
+app.use(htmlRoutes)
+
+// Starts the server to begin listening
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
