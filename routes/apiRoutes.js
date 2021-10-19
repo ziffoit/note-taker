a9b98a0cfc07fe6fdffef2b233f897079a051352
Kevin Hanson <ziffoit@gmail.com>
2021-10-19 16:04:07 -0600

    .get efforting

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,14 @@
-const router = require("express").Router();
+const express = require("express");
+const app = express();
 const fs = require("fs");
 const notes = require("../db/db.json");
 const uuid = require("uuid/v1");
 
-router.get("/api/notes", (req, res) => {
+app.get("/api/notes", (req, res) => {
     res.json(results)
 });
 
-router.post("/api/notes", (req, res) => {
+app.post("/api/notes", (req, res) => {
     const newNote = req.body;
     newNote.id = uuid();
     notes.push(newNote);
@@ -21,7 +22,7 @@ router.post("/api/notes", (req, res) => {
     res.end();
 });
 
-router.delete(`/api/notes/:id`, (req, res) => {
+app.delete(`/api/notes/:id`, (req, res) => {
     console.log(req.params.id);
         
     for (let i = 0; i < notes.length; i++) {
@@ -41,4 +42,4 @@ router.delete(`/api/notes/:id`, (req, res) => {
 });
 
 // Starts the server to begin listening
-module.exports = router;
+module.exports = app;
